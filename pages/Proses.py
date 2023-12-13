3ed413b1a71b3873228621c64fac3d883cdebf4f
arosyihuddin <arosyihuddin6@gmail.com>
2023-12-13 18:42:27 +0700

    update

--- pages/Proses.py
+++ pages/Proses.py
@@ -13,6 +13,15 @@ selected = option_menu(
     icons=["data", "Process", "model", "implemen", "Test", "sa"],
     orientation="horizontal",
 )
+if "df" not in st.session_state:
+    print("OKE")
+    st.session_state.df = pd.read_csv("data/data_preprocessing.csv")
+    st.session_state.df_onehot = pd.read_csv("data/OneHotEncoder.csv")
+    st.session_state.df_tf_idf = pd.read_csv("data/TF IDF.csv")
+    st.session_state.df_tf = pd.read_csv("data/Term Frequensi.csv")
+    st.session_state.df_lf = pd.read_csv("data/Logarithm Frequensi.csv")
+    st.session_state.df_lda = pd.read_csv('data/reduksi dimensi.csv')
+
 if selected == "Crawling":
     st.write(
         'Dataset yang di ambil dari website pta.trunojoyo.ac.id dengan proses pengambilan menggunakan metode Crawling berikut adalah link colab untukk Cerawling datasetnya [Link Colab](https://colab.research.google.com/drive/1vHaVvp_g6eTXenmv8A44eJ0F8Zb3vCuz?usp=sharing)')
@@ -25,44 +34,38 @@ elif selected == "Preprocessing":
              3. Stopword Removal
              4. Stemming (Opsional)""")
     st.write("berikut hasil dari proses preprocesing:")
-    df = pd.read_csv("data/data_preprocessing.csv")
-    st.write(df)
+    st.write(st.session_state.df)
 
 elif selected == "VSM":
     onehot, tf_idf, tf, lf = st.tabs(['One Hot Encoding', 'TF IDF',
                                       'Term Frequensi', 'Logaritm Frequency'])
 
     with onehot:
-        df_onehot = pd.read_csv("data/OneHotEncoder.csv")
-        st.write(df_onehot)
+        st.dataframe(st.session_state.df_onehot)
 
     with tf_idf:
-        df_tf_idf = pd.read_csv("data/TF IDF.csv")
-        st.write(df_tf_idf)
+        st.dataframe(st.session_state.df_tf_idf)
 
     with tf:
-        df_tf = pd.read_csv("data/Term Frequensi.csv")
-        st.write(df_tf)
+        st.dataframe(st.session_state.df_tf)
 
     with lf:
-        df_lf = pd.read_csv("data/Logarithm Frequensi.csv")
-        st.write(df_lf)
+        st.dataframe(st.session_state.df_lf)
 
 elif selected == "LDA":
     st.write("hasil reduksi dimensi :")
-    df_lda = pd.read_csv('data/reduksi dimensi.csv')
-    st.write(df_lda)
+    st.dataframe(st.session_state.df_lda)
 
 else:
     model = joblib.load("model/naive bayes.pkl")
     lda = joblib.load("model/lda.pkl")
     vectorizer = joblib.load("model/tf_vectorizer.pkl")
 
-    abstrak = st.text_input("Masukkan Abstrak")
+    abstrak = [st.text_input("Masukkan Abstrak")]
     button = st.button("Klasifikasikan")
 
     if button:
-        x_new = vectorizer.transform([abstrak]).toarray()
+        x_new = vectorizer.transform(abstrak).toarray()
         lda_x = lda.transform(x_new)
         predictions = model.predict(lda_x)
         st.write("Abstrak termasuk dalam Kategori : ", predictions[0])
